4654442993dec7b13bb2b8859ea95f2eeceb4c94
Prohli <maximilian.prohl@gmail.com>
2018-04-13 11:00:32 +0200

    Kein PLan
    was ich hier tue

--- app/src/main/java/maxundmax/holiday4friends/MainActivity.java
+++ app/src/main/java/maxundmax/holiday4friends/MainActivity.java
@@ -1,10 +1,14 @@
 package maxundmax.holiday4friends;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,21 +18,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentListenOptions;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.ServerTimestamp;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private static final int RC_SIGN_IN = 123;
+    private FirebaseFirestore mFirestore;
+    private static final String TAG = "MainActivity";
+    private static final String DEFAULT_COLLECTION = "Urlaube";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +93,8 @@ public class MainActivity extends AppCompatActivity
                 RC_SIGN_IN);
 
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -72,9 +104,36 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        findViewById(R.id.button_write).setOnClickListener(this);
+        mFirestore = FirebaseFirestore.getInstance();
+
+        NavigationView logout = findViewById(R.id.logout);
+
 
     }
 
+    private void onWriteClicked() {
+        EditText holiday_name = (EditText)findViewById(R.id.holiday_name);
+        EditText holiday_story = (EditText)findViewById(R.id.holiday_story);
+        String holidayName = holiday_name.getText().toString();
+        String holidayStory = holiday_story.getText().toString();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", holidayName);
+        map.put("story", holidayStory);
+
+        Task<Void> voidTask = mFirestore.collection(DEFAULT_COLLECTION)
+                .document()
+                .set(map)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "write:onComplete");
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "write:onComplete:failed", task.getException());
+                        }
+                    }
+                });
+    }
 
     @Override
     public void onBackPressed() {
@@ -93,6 +152,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -110,7 +170,6 @@ public class MainActivity extends AppCompatActivity
             }
         }
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -151,4 +210,18 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+<<<<<<< HEAD
+    public void logout(){
+
+=======
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_write) {
+            onWriteClicked();
+        }
+>>>>>>> 96680075d7bbd3d27c316857c643d93a9281eaaa
+
+    }
 }
+
